82a0b2b6ecb2ee29c05673eb71ae47d80b7c6c96
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-11-06 20:59:13 +0100

    added background colour

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <div className="py-32 bg-nav-background shadow-lg flex justify-center">
+        <h2>Footer Section</h2>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
