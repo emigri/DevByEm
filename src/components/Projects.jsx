6c05a0bf6a2611eb6d3f1db507cf6f1eafdbc8a1
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-11-06 09:15:45 +0100

    changed background colour temporarily

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -72,33 +72,33 @@ function Projects() {
 
   return (
     <>
-      <div id="projects" className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="sr-only">Products</h2>
-
-          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {ProjectCards.map((Projects) => (
-              <a
-                key={Projects.id}
-                href={Projects.href}
-                target="_blank"
-                rel="noreferrer"
-                className="group"
-              >
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-400 xl:aspect-h-8 xl:aspect-w-7 shadow-sm">
-                  <img
-                    alt={Projects.imageAlt}
-                    src={Projects.imageSrc}
-                    className="h-full w-full object-cover object-center group-hover:opacity-20 duration-500 shadow-xl"
-                  />
-                </div>
-                <h3 className="mt-4 text-sm text-gray-700">{Projects.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">
-                  {Projects.description}
-                </p>
-              </a>
-            ))}
-          </div>
+      <div
+        id="projects"
+        className=" bg-rose-100 mx-auto max-w-2xl px-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
+      >
+        <h2 className="sr-only">Products</h2>
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {ProjectCards.map((Projects) => (
+            <a
+              key={Projects.id}
+              href={Projects.href}
+              target="_blank"
+              rel="noreferrer"
+              className="group"
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-400 xl:aspect-h-8 xl:aspect-w-7 shadow-sm">
+                <img
+                  alt={Projects.imageAlt}
+                  src={Projects.imageSrc}
+                  className="h-full w-full object-cover object-center group-hover:opacity-20 duration-500 shadow-xl"
+                />
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">{Projects.name}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                {Projects.description}
+              </p>
+            </a>
+          ))}
         </div>
       </div>
     </>
