570cba8fa0115ddeda9f6f471efea7d693e4f7a4
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-11-06 09:14:22 +0100

    added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+function Hero() {
+  const navigation = [
+    { name: "Home", href: "/home" },
+    { name: "Projects", href: "/projects" },
+    { name: "About Me", href: "/about" },
+    { name: "Contact", href: "/contact" }
+  ];
+
+  return (
+    <>
+      {/* Navbar */}
+      <header>
+        <nav
+          aria-label="Global"
+          className="flex p-6 px-8 shadow-lg relative isolate "
+        >
+          <div className="flex justify-center sm:flex-1 md:flex-1 lg:flex-1">
+            <div className="flex sm:flex gap-x-6 sm:duration-200 md:flex md:gap-x-12 md:duration-200 lg:flex lg:gap-x-20 lg:duration-200">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.href}
+                  className="text-sm font-semibold leading-8 text-gray-900 md:text-lg lg:text-xl"
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+export default Hero;
