b059da0b4f35355c8b850aff0153e26ae65d61a5
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-11-06 09:11:45 +0100

    added routes to each componant instead of having them all one page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
+      <Router>
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
