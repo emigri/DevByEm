294e65fdec69d821bff7f8cfd055faf365f8593d
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-11-13 21:45:10 +0100

    added project cards using props

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
+import ProjectCard from "./ProjectCard.jsx";
 import teaMakerThumbnail from "../assets/tea-maker-thumbnail.png";
 import weatherDashboardThumbnail from "../assets/weather-dashboard-thumbnail.png";
 import passwordGeneratorThumbnail from "../assets/password-generator-thumbnail.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function Projects() {
   const ProjectCards = [
@@ -76,29 +79,10 @@ function Projects() {
         <h1 className="text-balance p-28 text-center text-4xl font-bold tracking-tight text-gray-800 sm:text-6xl">
           Projects
         </h1>
-        <div className="flex-col flex items-center mx-auto max-w-2xl">
+        {/* Project cards */}
+        <div className="grid gap-6 grid-cols-2">
           {ProjectCards.map((Projects) => (
-            <div className="hover:scale-105 hover:duration-700 transition duration-1000">
-              <a
-                key={Projects.id}
-                href={Projects.href}
-                target="_blank"
-                rel="noreferrer"
-                className="card h-[100px]"
-              >
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-400 xl:aspect-h-8 xl:aspect-w-7 shadow-lg">
-                  <img
-                    alt={Projects.imageAlt}
-                    src={Projects.imageSrc}
-                    className="h-full w-full object-cover object-center group-hover:opacity-20 duration-500 shadow-xl"
-                  />
-                </div>
-                <h3 className="mt-4 text-sm text-gray-700">{Projects.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">
-                  {Projects.description}
-                </p>
-              </a>
-            </div>
+            <ProjectCard ProjectCard={Projects} />
           ))}
         </div>
       </div>
